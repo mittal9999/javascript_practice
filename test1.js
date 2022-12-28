203c69cd8185809de2f82e6267771cd4ddde4265
mittal9999 <mittalrathva99@gmail.com>
2022-12-29 00:35:41 +0530

    first commit

---
+++ test1.js
@@ -0,0 +1,28 @@
+// cut array length
+
+// let  data = [4,7,1,4,9,2,4,1];
+// data.length= 2;
+// console.warn(data)
+
+//sum of array element
+// let sum = data.reduce((x,y) => x+y);
+// console.warn(sum);
+
+// remove  dublicate value of given array using set
+// let unique =  new Set(data);
+// // it's called destructore
+// console.log([...unique]);
+
+// comm operator
+// let x=10;
+// y = 40;
+// x=(x++,x);
+// x=(x+=20,x);
+// x=(x+=20,3);
+// x=(x+=20,y);
+// console.log(x);
+
+// value swap using destucturing
+let x=20,y=30;
+[x,y]=[y,x];
+console.log(x,y);
